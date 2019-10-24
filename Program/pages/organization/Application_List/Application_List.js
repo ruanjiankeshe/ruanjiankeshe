52714f502c9df9e22a17226864865b5c41deab42
yourname <1378650128@qq.com>
2019-10-24 21:22:28 +0800

    (result)

---
+++ Program/pages/organization/Application_List/Application_List.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    listData: [
+      { "CouInfo": "", "PId": "", "PTelephone": "", "TeaId": "", "CouDay": "" }
+    ]
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+
+})
